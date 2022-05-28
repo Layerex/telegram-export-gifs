d1147554dcb10b981c0e08e5c6f9052c35dab22c
Layerex <layerex@dismail.de>
2022-05-28 10:41:49 +0300

    Check if arguments are provided more than one time

--- cli.go
+++ cli.go
@@ -39,9 +39,15 @@ func ParseArgs() Args {
 		}
 		switch os.Args[i] {
 		case "-d", "--directory":
+			if args.Directory != "" {
+				panic("--directory option is provided more than one time")
+			}
 			nextArg()
 			args.Directory = os.Args[i]
 		case "--app-id":
+			if args.AppID != 0 {
+				panic("--app-id option is provided more than one time")
+			}
 			nextArg()
 			argument, err := strconv.Atoi(os.Args[i])
 			if err != nil {
@@ -49,6 +55,9 @@ func ParseArgs() Args {
 			}
 			args.AppID = int32(argument)
 		case "--app-hash":
+			if args.AppHash != "" {
+				panic("--app-hash option is provided more than one time")
+			}
 			nextArg()
 			if len(os.Args[i]) != 32 || !IsHex(os.Args[i]) {
 				panic("--app-hash value has to be a hex string of 32 characters")
